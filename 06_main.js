e11fbaf50e83af51a17688b1dbcb45dc87489515
lroos27 <leonie.roos@t-online.de>
2021-08-13 20:06:35 +0100

    update the serverAppURL

--- 06_main.js
+++ 06_main.js
@@ -31,7 +31,7 @@ $("document").ready(function() {
         // specifying all information for the deployment
         deploy: {
             experimentID: "278",
-            serverAppURL: "magpie-demo.herokuapp.com/api/submit_experiment/",
+            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
